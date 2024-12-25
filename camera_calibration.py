f5195fb33a5b56137de1d544d8011a6c054bd74e
mowibox <ousmane.thiongane@ensea.fr>
2024-12-25 04:01:43 +0100

    Adding yaml and args

--- camera_calibration.py
+++ camera_calibration.py
@@ -8,17 +8,19 @@
 """
 
 # Imports
+import sys
 import cv2
 import yaml
 import time
+import argparse
 import threading
 import numpy as np
 from flask import Flask, Response
 
 CHESSBOARD_SIZE = (7, 10) # Number of inners corners (row, col)
 SQUARE_SIZE = 0.025       # In meters (25 mm)
-N_IMAGES = 0              # Counter for the number of chessboards detections
-TIME_DELAY = 3            # Minimal itme between two detections
+N_IMAGES = 0              # Counter for the number of chessboard detections
+TIME_DELAY = 3            # Minimal time between two detections
 
 objp = np.zeros((CHESSBOARD_SIZE[0]*CHESSBOARD_SIZE[1], 3), np.float32)
 objp[:, :2] = np.mgrid[0:CHESSBOARD_SIZE[0], 0:CHESSBOARD_SIZE[1]].T.reshape(-1,2)*SQUARE_SIZE
@@ -47,9 +49,35 @@ class CameraThread(threading.Thread):
         self.running = False
 
 
-def camera_calibration():
+def parse_args():
+    parser = argparse.ArgumentParser(description="Camera calibration")
+    parser.add_argument('--max_img', type=int, default=15, help='Number of images to capture before stopping calibration.')
+    args = parser.parse_args()
+    return args
+
+
+def save_calibration(camera_matrix, dist_coeffs):
+    """
+    Saves the camera calibration parameters in a YAML file
+
+    @param: camera_matrix: The camera matrix
+    @param: dist_coeffs: The distorsion coefficients
+    """
+    calibration_data = {
+        'camera_matrix': camera_matrix.tolist(),
+        'distorsion_coefficients': dist_coeffs.tolist()
+    }
+    with open("cam_params.yaml", "w") as yaml_file:
+        yaml.dump(calibration_data, yaml_file, default_flow_style=False)
+        print("Calibration data has been saved to 'cam_params.yaml'.")
+
+
+
+def camera_calibration(MAX_IMAGES):
     """
     Performs chessboard camera calibration
+
+    @param MAX_IMAGES: The number of images to capture before stopping calibration
     """
     global N_IMAGES, objpoints, imgpoints, last_detection_time
 
@@ -78,11 +106,12 @@ def camera_calibration():
                     N_IMAGES+=1
                     last_detection_time = current_time
 
-            n_img_text = f"num_of_imgs: {N_IMAGES}"
+            n_img_text = f"num_of_imgs: {N_IMAGES}/{MAX_IMAGES}"
             cv2.putText(frame, n_img_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (65, 55, 255), 2, cv2.LINE_AA)
 
 
-            ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+            ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+            time.sleep(0.1)
             if not ret:
                 continue
         
@@ -90,13 +119,15 @@ def camera_calibration():
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
             
-            if N_IMAGES >= 10:
+            if N_IMAGES >= MAX_IMAGES:
                 break
 
     if len(objpoints) > 0:
         ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
         print(f"Camera Matrix:\n{camera_matrix}")
         print(f"Distorsion coefficients:\n{dist_coeffs}")
+        save_calibration(camera_matrix, dist_coeffs)
+        print("Press CTRL-C to close the terminal...")
 
     camera.release()
     camera_thread.stop()
@@ -104,7 +135,8 @@ def camera_calibration():
 
 @app.route('/')
 def video_feed():
-    return Response(camera_calibration(),
+    MAX_IMAGES = parse_args().max_img
+    return Response(camera_calibration(MAX_IMAGES),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
