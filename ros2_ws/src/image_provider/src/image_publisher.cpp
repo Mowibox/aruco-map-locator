a761877a979e1adf9d030f4bbf87695afc10dd9b
mowibox <ousmane.thiongane@ensea.fr>
2024-12-26 21:35:11 +0100

    Moving yaml extraction to subscriber

--- ros2_ws/src/image_provider/src/image_publisher.cpp
+++ ros2_ws/src/image_provider/src/image_publisher.cpp
@@ -1,13 +1,11 @@
-#include <yaml-cpp/yaml.h>
 #include <rclcpp/rclcpp.hpp>
 #include <opencv2/opencv.hpp>
 #include <std_msgs/msg/header.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
-const std::string CALIBRATION_FILE = "../cam_params.yaml";
 // Frame dimensions
-const int FRAME_WIDTH = 640;
-const int FRAME_HEIGHT = 480;
+const int FRAME_WIDTH = 320;
+const int FRAME_HEIGHT = 240;
 
 sensor_msgs::msg::Image::SharedPtr mat_to_image_msg(const cv::Mat &frame, const rclcpp::Clock::SharedPtr &clock) {
    /** 
@@ -31,10 +29,10 @@ sensor_msgs::msg::Image::SharedPtr mat_to_image_msg(const cv::Mat &frame, const 
 
 class ImagePublisher : public rclcpp::Node{
     public:
-    ImagePublisher(cv::VideoCapture &capture, const cv::Mat & camera_matrix, const cv::Mat &dist_coeffs)
-        : Node("image_publisher"), capture_(capture), camera_matrix_(camera_matrix), dist_coeffs_(dist_coeffs) {
-            publisher_ = this->create_publisher<sensor_msgs::msg::Image>("camera_feed", 10);
-            timer_ = this->create_wall_timer(std::chrono::duration<double>(0.1), std::bind(&ImagePublisher::publish_frame, this));
+    ImagePublisher(cv::VideoCapture &capture)
+        : Node("image_publisher"), capture_(capture) {
+            publisher_ = this->create_publisher<sensor_msgs::msg::Image>("camera_feed", 20);
+            timer_ = this->create_wall_timer(std::chrono::duration<double>(0.2), std::bind(&ImagePublisher::publish_frame, this));
     }
 
     private:
@@ -48,63 +46,18 @@ class ImagePublisher : public rclcpp::Node{
             return;
         }
 
-        cv::Mat corrected_frame;
-        cv::Mat new_camera_matrix = cv::getOptimalNewCameraMatrix(camera_matrix_, dist_coeffs_, frame.size(), 1);
-        cv::undistort(frame, corrected_frame, camera_matrix_, dist_coeffs_, new_camera_matrix);
-
-        auto msg = mat_to_image_msg(corrected_frame, this->get_clock());
-
+        auto msg = mat_to_image_msg(frame, this->get_clock());
         publisher_->publish(*msg);
     }
 
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     cv::VideoCapture &capture_;
-    cv::Mat camera_matrix_;
-    cv::Mat dist_coeffs_;
-        
 };
 
-bool load_calibration_params(const std::string &filepath, cv::Mat &camera_matrix, cv::Mat &dist_coeffs){
-   /** 
-    * Loads the camera calibration parameters specified in the provided yaml file
-    *@param filepath: The yaml file path 
-    *@param camera_matrix: The camera matrix
-    *@param dist_coeffs: The distortion coefficientss
-    */
-    try{
-        YAML::Node config = YAML::LoadFile(filepath);
-        auto camera_matrix_data = config["camera_matrix"].as<std::vector<std::vector<double>>>();
-        camera_matrix = cv::Mat(3, 3, CV_64F);
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                camera_matrix.at<double>(i, j) = camera_matrix_data[i][j];
-            }
-        }
-
-        auto dist_coeffs_data = config["distorsion_coefficients"].as<std::vector<std::vector<double>>>();
-        dist_coeffs = cv::Mat(dist_coeffs_data.size(), dist_coeffs_data[0].size(), CV_64F);
-        for (size_t i = 0; i < dist_coeffs_data.size(); ++i) {
-            for (size_t j = 0; j < dist_coeffs_data[i].size(); ++j) {
-                dist_coeffs.at<double>(i, j) = dist_coeffs_data[i][j];
-            }
-        }
-
-        return true;
-
-    } catch (const YAML::Exception &e){
-        std::cerr << "Failed to load calibraiton parameters." << e.what() << std::endl;
-        return false;
-    }
-}
 
 int main(int argc, char **argv){
     rclcpp::init(argc, argv);
-    
-    cv::Mat camera_matrix, dist_coeffs;
-    if (!load_calibration_params(CALIBRATION_FILE, camera_matrix, dist_coeffs)){
-        return EXIT_FAILURE;
-    }
 
     cv::VideoCapture capture(0);
     if (!capture.isOpened()){
@@ -115,7 +68,7 @@ int main(int argc, char **argv){
     capture.set(cv::CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
     capture.set(cv::CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
 
-    auto node = std::make_shared<ImagePublisher>(capture, camera_matrix, dist_coeffs);
+    auto node = std::make_shared<ImagePublisher>(capture);
     rclcpp::spin(node);
 
     rclcpp::shutdown();
--- ros2_ws/src/image_provider/src/image_subscriber.cpp
+++ ros2_ws/src/image_provider/src/image_subscriber.cpp
@@ -1,6 +1,42 @@
+#include <yaml-cpp/yaml.h>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <opencv2/opencv.hpp>
+
+const std::string CALIBRATION_FILE = "../cam_params.yaml";
+
+bool load_calibration_params(const std::string &filepath, cv::Mat &camera_matrix, cv::Mat &dist_coeffs){
+   /** 
+    * Loads the camera calibration parameters specified in the provided yaml file
+    *@param filepath: The yaml file path 
+    *@param camera_matrix: The camera matrix
+    *@param dist_coeffs: The distortion coefficientss
+    */
+    try{
+        YAML::Node config = YAML::LoadFile(filepath);
+        auto camera_matrix_data = config["camera_matrix"].as<std::vector<std::vector<double>>>();
+        camera_matrix = cv::Mat(3, 3, CV_64F);
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                camera_matrix.at<double>(i, j) = camera_matrix_data[i][j];
+            }
+        }
+
+        auto dist_coeffs_data = config["distorsion_coefficients"].as<std::vector<std::vector<double>>>();
+        dist_coeffs = cv::Mat(dist_coeffs_data.size(), dist_coeffs_data[0].size(), CV_64F);
+        for (size_t i = 0; i < dist_coeffs_data.size(); ++i) {
+            for (size_t j = 0; j < dist_coeffs_data[i].size(); ++j) {
+                dist_coeffs.at<double>(i, j) = dist_coeffs_data[i][j];
+            }
+        }
+
+        return true;
+
+    } catch (const YAML::Exception &e){
+        std::cerr << "Failed to load calibraiton parameters." << e.what() << std::endl;
+        return false;
+    }
+}
 
 class ImageSubscriber : public rclcpp::Node {
     public:
@@ -10,6 +46,10 @@ class ImageSubscriber : public rclcpp::Node {
 
         subscriber_ = this->create_subscription<sensor_msgs::msg::Image>(
             topic, 10, std::bind(&ImageSubscriber::image_callback, this, std::placeholders::_1));
+        
+        if (!load_calibration_params(CALIBRATION_FILE, camera_matrix_, dist_coeffs_)){
+        return;
+        }
 
         RCLCPP_INFO(this->get_logger(), "Seeing the topic '%s'", topic.c_str()); 
     }
@@ -21,7 +61,12 @@ class ImageSubscriber : public rclcpp::Node {
         */
         try{
             cv::Mat frame(msg->height, msg->width, CV_8UC3, const_cast<unsigned char*>(msg->data.data()), msg->step);
-            cv::imshow("Video", frame);
+
+            cv::Mat corrected_frame;
+            cv::Mat new_camera_matrix = cv::getOptimalNewCameraMatrix(camera_matrix_, dist_coeffs_, frame.size(), 1);
+            cv::undistort(frame, corrected_frame, camera_matrix_, dist_coeffs_, new_camera_matrix);
+
+            cv::imshow("Video", corrected_frame);
             cv::waitKey(1);
         } catch (const std::exception &e) {
             RCLCPP_ERROR(this->get_logger(), "Error processing image: %s", e.what());
@@ -29,11 +74,16 @@ class ImageSubscriber : public rclcpp::Node {
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscriber_;
+    cv::Mat camera_matrix_;
+    cv::Mat dist_coeffs_;
 
 };
 
+
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
+    
+    cv::Mat camera_matrix, dist_coeffs;
     auto node = std::make_shared<ImageSubscriber>();
     rclcpp::spin(node);
     rclcpp::shutdown();
